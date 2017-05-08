dc07217c5d2f347824c1ac76d2a58991dc500a13
vsouza <vinicius.rj@gmail.com>
2017-05-08 17:02:09 -0300

    implementing jest

---
+++ __tests__/get-tenant.test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var getTenant = require('../lib/get-tenant');
+
+test('', () => {
+    let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5hbnRJRCI6InRlc3RlIn0.j-NUJF8fbeV5fDtASOu3QZPXmoeo_tZFAHbGAwGbJe4";
+    expect.assertions(1);
+    return getTenant('secret')([token, "A", 123, {"name": "B"}]).then(data => expect(data).toContain('teste'));
+});
