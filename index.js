89c6024daa49d34dc9c75c01fd43768d1a521bcb
vsouza <vinicius.rj@gmail.com>
2017-05-10 16:20:30 -0300

    refactory resources and services

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 /**
  * Generate a generic C.R.U.D. api using restify and sequelize-multi-tenancy
-<<<<<<< HEAD
- * @param {server} restifyServer
- * @param {string} modelName
- * @param {models} multiTenancySequelize
- * @param {logger} bunnyanLogger
-=======
->>>>>>> 3dc94feddaad1646bafa18889abcdc5a57c66ed3
  * @return {object}
  */
 module.exports = (function() {
@@ -24,13 +17,15 @@ module.exports = (function() {
             services: {
                 initDb: require('./lib/services/init-db')
             },
-            getModelById: getModelById,
-            listAllModels: listAllModels,
-            createModel: createModel,
-            updateModel: updateModel,
-            deleteModel: deleteModel,
-            initDB: initDB,
-            setupAPI: function(server, modelName, models, logger){
+            resources: {
+                getModelById: getModelById,
+                listAllModels: listAllModels,
+                createModel: createModel,
+                updateModel: updateModel,
+                deleteModel: deleteModel,
+                initDB: initDB,
+            },
+            setupAPI: function(appAuthSecret, server, modelName, models, logger){
                 server.get(modelName + '/:id',  getModelById(server, modelName, models, logger));
                 server.put(modelName,           validating(server, modelName, models), createModel(server, modelName, models, logger));
                 server.post(modelName,          validating(server, modelName, models), updateModel(server, modelName, models, logger));
