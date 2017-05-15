46104508525857bca11e55e0b0a2fb5a73854589
vsouza <vinicius.rj@gmail.com>
2017-05-14 23:49:38 -0300

    fix bug setupAPI

--- index.js
+++ index.js
@@ -31,12 +31,12 @@ module.exports = (function() {
                 initDB: initDB,
             },
             setupAPI: function(appAuthSecret, server, modelName, models, logger){
-                server.get(modelName + '/:id',  getModelById(server, modelName, models, logger));
-                server.put(modelName,           validating(server, modelName, models), createModel(server, modelName, models, logger));
-                server.post(modelName,          validating(server, modelName, models), updateModel(server, modelName, models, logger));
-                server.get(modelName,           listAllModels(server, modelName, models, logger));
-                server.post(modelName,          listAllModels(server, modelName, models, logger));
-                server.del(modelName + '/:id',  deleteModel(server, modelName, models, logger));
+                server.get(modelName + '/:id',  getModelById(appAuthSecret, server, modelName, models, logger));
+                server.put(modelName,           validating(server, modelName, models), createModel(appAuthSecret, server, modelName, models, logger));
+                server.post(modelName,          validating(server, modelName, models), updateModel(appAuthSecret, server, modelName, models, logger));
+                server.get(modelName,           listAllModels(appAuthSecret, server, modelName, models, logger));
+                server.post(modelName,          listAllModels(appAuthSecret, server, modelName, models, logger));
+                server.del(modelName + '/:id',  deleteModel(appAuthSecret, server, modelName, models, logger));
             }
         }
 })();
