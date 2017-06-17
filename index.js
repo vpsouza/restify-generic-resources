25e53e0eae9d5dc52904a23ee58dc45a0c5569e6
vsouza <vinicius.rj@gmail.com>
2017-06-17 17:19:23 -0300

    bug fix

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
 'use strict';
-import {createModel, deleteModel, getModelById, listAll, updateModel} from './lib/services';
-import * as resources from './lib';
+import * as services from './lib/services';
+import * as resources from './lib/resources';
 import validating from 'restify-sequelize-validation';
 
-export default {
-    services: {
-        createModel, 
-        deleteModel,
-        getModelById,
-        listAll,
-        updateModel
-    },
+const setupAPI = (appAuthSecret, server, modelName, models, logger) => {
+    server.get(modelName + '/:id',  resources.getModelById(server, modelName, models, logger));
+    server.put(modelName,           validating(server, modelName, models), resources.createModel(server, modelName, models, logger));
+    server.post(modelName,          validating(server, modelName, models), resources.updateModel(server, modelName, models, logger));
+    server.get(modelName,           resources.listAll(server, modelName, models, logger));
+    server.post(modelName,          resources.listAll(server, modelName, models, logger));
+    server.del(modelName + '/:id',  resources.deleteModel(server, modelName, models, logger));
+};
+
+export  {
+    services,
     resources,
-    setupAPI: function(appAuthSecret, server, modelName, models, logger){
-        server.get(modelName + '/:id',  resources.getModelById(server, modelName, models, logger));
-        server.put(modelName,           validating(server, modelName, models), resources.createModel(server, modelName, models, logger));
-        server.post(modelName,          validating(server, modelName, models), resources.updateModel(server, modelName, models, logger));
-        server.get(modelName,           resources.listAll(server, modelName, models, logger));
-        server.post(modelName,          resources.listAll(server, modelName, models, logger));
-        server.del(modelName + '/:id',  resources.deleteModel(server, modelName, models, logger));
-    }
+    setupAPI
 };
