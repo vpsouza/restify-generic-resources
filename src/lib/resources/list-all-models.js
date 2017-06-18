739d7c67d7797004ee5c1222d59ba43445a1bafe
vsouza <vinicius.rj@gmail.com>
2017-06-18 16:07:00 -0300

    bug fix

--- src/lib/resources/list-all-models.js
+++ src/lib/resources/list-all-models.js
@@ -4,8 +4,8 @@ import restify from 'restify';
 import {resolveSuccess, resolveError, getTenant} from 'easyutils';
 import {listAll} from '../services';
 
-export default (server, modelName, models, logger) =>
+export default (appAuthSecret, server, modelName, models, logger) =>
     (req, res) => 
-        [getTenant(process.env.AUTH_SECRET), listAll(server, modelName, models)].reduce((chain, task) => chain.then(task), Promise.resolve([req.params.jwt, req.body]))
+        [getTenant(appAuthSecret), listAll(server, modelName, models)].reduce((chain, task) => chain.then(task), Promise.resolve([req.params.jwt, req.body]))
             .then((result) => resolveSuccess(res, result))
             .catch(err => resolveError(res, new restify.InternalServerError(err), logger));
